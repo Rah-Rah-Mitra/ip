f799e2ed6c249e41355230729d0e45e006af5b5e
Rahul Mitra <115509410+Rah-Rah-Mitra@users.noreply.github.com>
2025-08-20 18:20:01 +0800

    feat: Update Jett Varkis messages and implement Level 1

--- src/main/java/JettVarkis.java
+++ src/main/java/JettVarkis.java
@@ -1,20 +1,32 @@
+import java.util.Scanner;
+
 public class JettVarkis implements Bot {
+
     private final String name = "Jett Varkis";
 
     @Override
     public String greet() {
-        return "Hello! I'm " + name + ".\n" +
-                "How can I assist you today?";
+        return name + " at your service. What can I get for you?";
     }
 
     @Override
     public String exit() {
-        return "Bye. Hope to see you again soon!";
+        return "Leaving so soon? See you next time.";
     }
 
     public static void main(String[] args) {
         JettVarkis bot = new JettVarkis();
         System.out.println(bot.greet());
+
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = in.nextLine();
+        }
+
         System.out.println(bot.exit());
+        in.close();
     }
 }
