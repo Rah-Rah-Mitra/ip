08ef38f8af856852fc1aee6dd339ce1a0af6ec0b
Rahul Mitra <115509410+Rah-Rah-Mitra@users.noreply.github.com>
2025-08-26 23:40:29 +0800

    Add date and time understanding

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,20 +1,39 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
+    protected String originalBy;
+
+    public Deadline(String description, LocalDateTime by) {
+        super(description);
+        this.by = by;
+        this.originalBy = null;
+    }
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = null;
+        this.originalBy = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        if (by != null) {
+            return "[D]" + super.toString() + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma")) + ")";
+        } else {
+            return "[D]" + super.toString() + " (by: " + originalBy + ")";
+        }
     }
 
     @Override
     public String toFileString() {
-        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        if (by != null) {
+            return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        } else {
+            return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + originalBy;
+        }
     }
 }
 
