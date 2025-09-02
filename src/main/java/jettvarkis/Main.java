b6eb3d29f323ba15ea25bbeb097ce823d0eb79f4
Rahul Mitra <115509410+Rah-Rah-Mitra@users.noreply.github.com>
2025-09-02 22:50:26 +0800

    Add application entry point and GUI setup

---
+++ src/main/java/jettvarkis/Main.java
@@ -0,0 +1,32 @@
+package jettvarkis;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import jettvarkis.gui.MainWindow;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    private JettVarkis jettVarkis = new JettVarkis("../data/jettvarkis.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setJettVarkis(jettVarkis);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
