1c1a2cbdcb5769f643badd7780f6af51ca3f6f97
Rahul Mitra <115509410+Rah-Rah-Mitra@users.noreply.github.com>
2025-09-10 17:43:47 +0800

    refactor: Refactor UnmarkCommand to use UpdateStatusCommand
    Refactors the `UnmarkCommand` to extend the `UpdateStatusCommand`. This simplifies the `UnmarkCommand` by removing the need for its own `execute` method and instead providing an implementation for the `updateTaskStatus` and `showResult` methods.

--- src/main/java/jettvarkis/command/UnmarkCommand.java
+++ src/main/java/jettvarkis/command/UnmarkCommand.java
@@ -1,69 +1,42 @@
 package jettvarkis.command;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import jettvarkis.TaskList;
-import jettvarkis.exception.JettVarkisException;
-import jettvarkis.storage.Storage;
 import jettvarkis.task.Task;
 import jettvarkis.ui.Ui;
 
 /**
  * Represents an Unmark command. This command unmarks one or more tasks as not done.
  */
-public class UnmarkCommand extends Command {
-
-    private final int[] taskIndices;
+public class UnmarkCommand extends UpdateStatusCommand {
 
     /**
      * Constructs an UnmarkCommand with the specified task indices.
      *
-     * @param taskIndices
-     *            The zero-based indices of the tasks to be unmarked.
+     * @param taskIndices The zero-based indices of the tasks to be unmarked.
      */
     public UnmarkCommand(int... taskIndices) {
-        assert taskIndices != null;
-        this.taskIndices = taskIndices;
+        super(taskIndices);
     }
 
     /**
-     * Executes the Unmark command.
-     * Unmarks the tasks at the specified indices and displays a message to the user.
-     * The changes are then saved to storage.
+     * Updates the status of the task by marking it as not done.
      *
-     * @param ui
-     *            The Ui object to interact with the user.
-     * @param tasks
-     *            The TaskList object containing the tasks.
-     * @param storage
-     *            The Storage object to save the tasks.
-     * @throws JettVarkisException
-     *             If any task index is invalid or a task is not found.
+     * @param task The task to be marked as not done.
      */
     @Override
-    public void execute(Ui ui, TaskList tasks, Storage storage) throws JettVarkisException {
-        assert ui != null;
-        assert tasks != null;
-        assert storage != null;
-        List<Task> unmarkedTasks = new ArrayList<>();
-        for (int taskIndex : taskIndices) {
-            assert taskIndex >= 0 && taskIndex < tasks.getTaskCount();
-            tasks.getTask(taskIndex).ifPresentOrElse(task -> {
-                task.markAsUndone();
-                unmarkedTasks.add(task);
-            }, () -> {
-                try {
-                    throw new JettVarkisException(JettVarkisException.ErrorType.TASK_NOT_FOUND);
-                } catch (JettVarkisException e) {
-                    ui.showError(e.getMessage());
-                }
-            });
-        }
-        if (!unmarkedTasks.isEmpty()) {
-            assert unmarkedTasks != null : "Unmarked tasks list should not be null";
-            ui.showUnmarkedTasks(unmarkedTasks);
-            storage.save(tasks.getTasks());
-        }
+    protected void updateTaskStatus(Task task) {
+        task.markAsUndone();
+    }
+
+    /**
+     * Displays the unmarked tasks to the user.
+     *
+     * @param ui The Ui object to display the result.
+     * @param tasks The list of tasks that were unmarked.
+     */
+    @Override
+    protected void showResult(Ui ui, List<Task> tasks) {
+        ui.showUnmarkedTasks(tasks);
     }
 }
